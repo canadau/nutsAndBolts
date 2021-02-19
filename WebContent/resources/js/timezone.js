c928b61868be9fedf036971a4a2db694a25cf8ab
malasemi <61808673+malasemi@users.noreply.github.com>
2021-02-19 17:48:56 -0500

    Change 3

--- WebContent/resources/js/timezone.js
+++ WebContent/resources/js/timezone.js
@@ -3,7 +3,44 @@ function showMessage(){
 };
 
 function timeNow(){
-	var d = new Date();
-	/*var n = d.getTimezoneOffset();*/
-	document.getElementById("time").innerHTML = d;		
+	var buttonValue = document.getElementById("timeButton").value;
+	
+	/*if(buttonValue = "Save Time"){
+		document.getElementById("timeButton").value="Get Time";
+	};*/
+	
+	var t = new Date();
+	var x = document.getElementById("savetime");
+	var pValue = x.innerHTML;
+	if (pValue == " ") {
+		$('#savetime').html("The Time Was Saved <br/><br/>" + t );
+		clearTime();
+		
+	} else {
+		document.getElementById("savetime").style.display = "";
+		$('#savetime').html( t );
+	}
+
+	
 };
+
+function clearTime(){
+	var xe = document.getElementById("savetime");
+	setTimeout(function(){ xe = document.getElementById("timeButton").value="Get Time"}, 2000);
+	setTimeout(function(){ $("#savetime").hide(2000)}, 2000);
+	setTimeout(function(){ $('#savetime0').html("Please click the button to get the time" )}, 2000);
+	
+};
+
+function alertFunc() {
+	  alert("Hello!");
+	};
+
+$("#ttt").click( function() {
+	var myTime;
+	var t = new Date();
+	$("#savetime").text(t);
+	myTime = setTimeout(alertFunc, 3000);
+	myTime = setTimeout("clearTime()", 3000);
+	
+});
