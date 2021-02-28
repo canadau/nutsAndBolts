01db7cfe38fc3fd316e1ff8e7fd93cb46f9953f4
malasemi <61808673+malasemi@users.noreply.github.com>
2021-02-28 17:36:24 -0500

    Add Products 2

---
+++ src/com/nutsandbolts/beans/AddProductsBeans.java
@@ -0,0 +1,86 @@
+package com.nutsandbolts.beans;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import com.nutsandbolts.tools.DBConnection;
+import com.nutsandbolts.tools.ShowMessages;
+
+@ManagedBean(name="addProductB")
+@RequestScoped
+public class AddProductsBeans implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int fSku;
+	private String fName;
+	private String fDescription;
+	private double fPrice;
+	
+	public AddProductsBeans() {}
+
+	public int getSku() {
+		return fSku;
+	}
+
+	public void setSku(int sku) {
+		this.fSku = sku;
+	}
+
+	public String getName() {
+		return fName;
+	}
+
+	public void setName(String name) {
+		this.fName = name;
+	}
+
+	public String getDescription() {
+		return fDescription;
+	}
+
+	public void setDescription(String description) {
+		this.fDescription = description;
+	}
+
+	public double getPrice() {
+		return fPrice;
+	}
+
+	public void setPrice(double price) {
+		this.fPrice = price;
+	}
+	
+	public void addProductToDB( ) {
+		Connection conn = null;
+		
+		try {
+			
+			String createSQL = "INSERT INTO products (sku, name, description, price) VALUES (?, ?, ?, ?)";
+			conn = DBConnection.getConnection();
+
+			PreparedStatement pst = conn.prepareStatement(createSQL);	
+			pst.setInt(1, fSku);
+			pst.setString(2, fName);
+			pst.setString(3, fDescription);
+			pst.setDouble(4, fPrice);
+
+			int rs = pst.executeUpdate();
+			
+			if (rs > 0) {
+				ShowMessages.showSuccessMessage("The Product Was added successfully");
+			} else {
+				 ShowMessages.showErrorMessage("Error, the product was not added, please try again"); 
+			}			
+				
+			pst.close();
+		} catch (Exception e) {
+			e.getCause();
+			System.out.println(e.getCause());
+		} finally {
+			DBConnection.close(conn);
+		}		
+		
+	}	
+
+}
--- src/com/nutsandbolts/beans/UpdateProductsBeans.java
+++ src/com/nutsandbolts/beans/UpdateProductsBeans.java
@@ -3,71 +3,88 @@ package com.nutsandbolts.beans;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
 import com.nutsandbolts.tools.DBConnection;
 import com.nutsandbolts.tools.ShowMessages;
 
-@ManagedBean(name="addProductB")
+@ManagedBean (name="updateProducts")
 @RequestScoped
-public class ProductsBeans implements Serializable {
+public class UpdateProductsBeans implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int fSku;
 	private String fName;
 	private String fDescription;
 	private double fPrice;
 	
-	public ProductsBeans() {}
+	
+	public UpdateProductsBeans() {}
 
-	public int getSku() {
+
+	public int getfSku() {
 		return fSku;
 	}
 
-	public void setSku(int sku) {
-		this.fSku = sku;
+
+	public void setfSku(int fSku) {
+		this.fSku = fSku;
 	}
 
-	public String getName() {
+
+	public String getfName() {
 		return fName;
 	}
 
-	public void setName(String name) {
-		this.fName = name;
+
+	public void setfName(String fName) {
+		this.fName = fName;
 	}
 
-	public String getDescription() {
+
+	public String getfDescription() {
 		return fDescription;
 	}
 
-	public void setDescription(String description) {
-		this.fDescription = description;
+
+	public void setfDescription(String fDescription) {
+		this.fDescription = fDescription;
 	}
 
-	public double getPrice() {
+
+	public double getfPrice() {
 		return fPrice;
 	}
 
-	public void setPrice(double price) {
-		this.fPrice = price;
+
+	public void setfPrice(double fPrice) {
+		this.fPrice = fPrice;
 	}
 	
-	public void addProductToDB( ) {
-		Connection conn = null;
-		
+	public void getProductsFromDB(int skuNumber) {
+		Connection conn = null;	
+		int count = 2;
 		try {
 			
-			String createSQL = "INSERT INTO products (sku, name, description, price) VALUES (?, ?, ?, ?)";
+			String createSQL = "SELECT sku, name, description, price FROM products WHERE sku = ?;";
 			conn = DBConnection.getConnection();
 
 			PreparedStatement pst = conn.prepareStatement(createSQL);	
-			pst.setInt(1, fSku);
-			pst.setString(2, fName);
-			pst.setString(3, fDescription);
-			pst.setDouble(4, fPrice);
-
-			int rs = pst.executeUpdate();
+			pst.setInt(1, skuNumber);
 			
-			if (rs > 0) {
+
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				fSku = rs.getInt(1);
+				fName = rs.getString(2);
+				fDescription = rs.getString(3);
+				fPrice = rs.getDouble(4);
+			}
+			
+			if (count > 0) {
 				ShowMessages.showSuccessMessage("The Product Was added successfully");
 			} else {
 				 ShowMessages.showErrorMessage("Error, the product was not added, please try again"); 
@@ -79,8 +96,9 @@ public class ProductsBeans implements Serializable {
 			System.out.println(e.getCause());
 		} finally {
 			DBConnection.close(conn);
-		}		
-		
-	}	
+		}	
+	}
+	
+	
 
 }
