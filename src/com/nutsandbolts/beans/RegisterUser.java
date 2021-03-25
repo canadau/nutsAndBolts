9f80171209f05bb9a591008dacf5eae7ccfd9eb7
Luke Foshizle <forshtrack@gmail.com>
2021-03-24 20:33:09 -0400

    Register User working?

--- src/com/nutsandbolts/beans/RegisterUser.java
+++ src/com/nutsandbolts/beans/RegisterUser.java
@@ -3,21 +3,35 @@ package com.nutsandbolts.beans;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import com.nutsandbolts.tools.DBConnection;
+import com.nutsandbolts.tools.EmailValidator;
+import com.nutsandbolts.tools.PasswordValidator;
 import com.nutsandbolts.tools.ShowMessages;
 
-@ManagedBean(name="addProductB")
+@ManagedBean (name = "registerUser")
 @RequestScoped
 public class RegisterUser implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+	
+	private int id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
 	public RegisterUser() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -50,28 +64,32 @@ public class RegisterUser implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	/*********** Methods start here ***********/
 	
-	public void addProductToDB( ) {
+	public boolean isExistUser() {
+		boolean stat = false;
 		Connection conn = null;
+		PreparedStatement pst = null;
+		int count = 0;
 		
 		try {
 			
-			String createSQL = "INSERT INTO userLogin (firstName, lastName, email, password) VALUES (?, ?, ?, ?)";
+			String createSQL = "SELECT email FROM userLogin WHERE email = ?;";
 			conn = DBConnection.getConnection();
 
-			PreparedStatement pst = conn.prepareStatement(createSQL);	
-			pst.setString(1, firstName);
-			pst.setString(2, lastName);
-			pst.setString(3, email);
-			pst.setString(4, password);
+			pst = conn.prepareStatement(createSQL);	
+			pst.setString(1, email);
 
-			int rs = pst.executeUpdate();
+			ResultSet rs = pst.executeQuery();
 			
-			if (rs > 0) {
-				ShowMessages.showSuccessMessage("The user was added successfully");
-			} else {
-				 ShowMessages.showErrorMessage("Error, the user was not added, please try again"); 
+			if (rs.next()) {
+				count++;				
 			}			
+			
+			if (count > 0) {				
+				stat = true; 				
+			} 
 				
 			pst.close();
 		} catch (Exception e) {
@@ -79,12 +97,61 @@ public class RegisterUser implements Serializable {
 			System.out.println(e.getCause());
 		} finally {
 			DBConnection.close(conn);
-		}		
+		}
+		return stat;
+	}
+	
+	public void registerUser() {
 		
-	}	
+		Connection conn = null;
+		PreparedStatement pst = null;
+		
+		if(!PasswordValidator.isValid(password)) {
+			
+			ShowMessages.showErrorMessage("Password must be at least one lowercase letter, "
+					+ "one uppercase letter, one number, a special character, and 8 characters long.");
+			return;
+			
+		} else if (!EmailValidator.isValidEmailAddress(email) || !EmailValidator.isLowercaseEmail(email)) {
+			
+			ShowMessages.showErrorMessage("The email must be lower case, and you need to use your employee email to complete this process, 'example@nab.com'");
+			return;
+			
+		} else if (!isExistUser()) {
+			try {
+				
+				String createSQL = "INSERT INTO userLogin (firstName, lastName, email, password) "
+						+ "VALUES (?,?,?,?);";
+				conn = DBConnection.getConnection();
+				firstName = firstName.trim().substring(0,1).toUpperCase() + firstName.substring(1);
+				lastName = lastName.trim().substring(0,1).toUpperCase() + lastName.substring(1);
+				email = email.trim().toLowerCase();
+				pst = conn.prepareStatement(createSQL);	
+				pst.setString(1, firstName);
+				pst.setString(2, lastName);
+				pst.setString(3, email);
+				pst.setString(4, password);
 
+				int rs = pst.executeUpdate();
+				
+				if (rs > 0) {
+					ShowMessages.showSuccessMessage("The account was created successfully");				
+				} else {
+					ShowMessages.showErrorMessage("Error, please try again!");
+				}
+					
+				pst.close();
+			} catch (Exception e) {
+				e.getCause();
+				System.out.println(e.getCause());
+			} finally {
+				DBConnection.close(conn);
+			}
+		} else {
+			ShowMessages.showErrorMessage("You already have an account please call customer support for help!");
+		}
+				
+	}
+	
 
 }
-
-
-
